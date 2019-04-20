3cf87198a9b72becbdfac797f677eb95b967ba16
XinWu <xinwuwh@163.com>
2019-04-20 20:22:22 +1000

    react with redux todolist

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,123 +1,34 @@
-import React, { Component, Fragment } from 'react';
-import TodoItem from './Todoitem'
-import axios from 'axios'
-import './style.css'
+import React, {Component} from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+import store from './store';
 
-class Todolist extends Component{
+
+class Todolist extends Component {
+
     constructor(props){
         super(props);
-        //this.state组件状态
-        this.state = {
-            inputValue:'',
-            list:['Study English','Study React'] //列表中的项
-        }
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.handleBtnClick = this.handleBtnClick.bind(this)
-        this.handleItemDelete = this.handleItemDelete.bind(this)
+        this.state= store.getState();
+        //console.log(this.state());
     }
-/*
-    //在组件即将被挂载到页面的时刻自动执行
-    componentWillMount(){
-        console.log('componentWillMount');
-    }
-*/
+
     render(){
-        //console.log('render')
-        return(
-            <Fragment>
-                <div>
-                    <label htmlFor="insertArea">Input Content</label>
-                
-                    <input 
-                    id="insertArea"
-                    className='input'
-                    value={this.state.inputValue} 
-                    onChange={this.handleInputChange}//监听事件改变
-                    ref={(input) => {this.input=input}}
-                    />
-                    <button onClick={this.handleBtnClick}>Submit</button></div>
-                <ul ref={(ul)=> {this.ul=ul}}> 
-                    {this.getTodoItem()}
-                </ul>
-            </Fragment>
+        return (
+            <div style={{marginTop: '10px', marginLeft:'10px'}}>
+                <div> 
+                    <Input value={this.state.value} placeholder='todo info' style={{width: '300px', marginRight:'10px'}}/>
+                    <Button type="primary">Submit</Button>
+                </div>
+                <List
+                    style={{marginTop: '10px', width: '300px'}}
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                />
+            </div>
+        
+
         )
     }
-
-
-    //组件被挂载之后自动执行（页面发ajax请求数据，数据拿到后，被挂载在页面上）
-    componentDidMount(){
-        axios.get('/api/todolist')
-        .then ((res)=>{
-            this.setState(() => ({
-                list: [...res.data]
-            }));
-        })
-        .catch(()=>{alert('error')})
-    }
-/*
-    //组件被更新之前，他会自动被执行
-    shouldComponentUpdate(){
-        console.log('shouldComponentUpdate');
-        return true;
-    }
-
-    //组建被更新之前，它会自动执行，但是它在shouldComponentUpdate之后执行，是否执行取决于返回结果
-    componentWillUpdate(){
-        console.log('ComponentWillUpdate');
-    
-    }
-
-    //组件更新完成之后会被执行
-    componentDidUpdate(){
-        console.log('ComponentDidUpdate');
-    
-    }
-*/
-
-    getTodoItem(){
-        return this.state.list.map((item,index) =>{
-            return (
-            <div key={index}>
-                <TodoItem 
-                
-                content={item} 
-                index={index}
-                deleteItem={this.handleItemDelete}
-                />
-            {/*父组件通过属性的形式向子组件传递数据，方法。方法需要bind(this)进行绑定 */}
-            </div>
-            )
-        })
-
-    }
-
-    handleInputChange(e){
-        
-        const value = e.target.value;
-        this.setState(() => ({
-            inputValue: value
-            }));
-    }
-
-    handleBtnClick(){
-        this.setState((prevState) =>({
-            list: [...prevState.list, prevState.inputValue],
-            inputValue: ''
-        }),() =>{
-            console.log(this.ul.querySelectorAll('div').length);  
-        });  
-          
-    }
-
-    handleItemDelete(index){
-        this.setState((prevState) => {
-            const list= [...prevState.list];
-            list.splice(index,1);
-            return {list}
-        });
-    }
-
 }
-
-//导出后才能使用组件
 export default Todolist;
