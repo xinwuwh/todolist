66ffeb4708d333679d0a6fd7c3ea5f044c5c4c84
XinWu <xinwuwh@163.com>
2019-04-21 09:23:16 +1000

    add todolistUI

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from './store';
 import {getInputChangeAction,getAddItemAction,getDeleteItemAction} from './store/actionCreators'
+import TodolistUI from './TodolistUI'
 
 class Todolist extends Component {
 
@@ -12,31 +12,20 @@ class Todolist extends Component {
         this.handleInputChange= this.handleInputChange.bind(this);
         this.handleStoreChange= this.handleStoreChange.bind(this);
         this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemDelete=this.handleItemDelete.bind(this);
         store.subscribe(this.handleStoreChange);
         //console.log(this.state());
     }
 
     render(){
         return (
-            <div style={{marginTop: '10px', marginLeft:'10px'}}>
-                <div> 
-                    <Input 
-                    value={this.state.inputValue} 
-                    placeholder='todo info' 
-                    style={{width: '300px', marginRight:'10px'}}
-                    onChange={this.handleInputChange}
-                    />
-                    <Button type="primary" onClick={this.handleBtnClick}>Submit</Button>
-                </div>
-                <List
-                    style={{marginTop: '10px', width: '300px'}}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
-                />
-            </div>
-        
-
+            <TodolistUI 
+                inputValue={this.state.inputValue}
+                list={this.state.list}
+                handleInputChange={this.handleInputChange}
+                handleBtnClick={this.handleBtnClick}
+                handleItemDelete={this.handleItemDelete}
+            />
         )
     }
 
