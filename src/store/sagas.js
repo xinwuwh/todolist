6ca202330001c06497a25ac1c2fdfc57d85b41d4
XinWu <xinwuwh@163.com>
2019-04-22 10:42:26 +1000

    Update sagas.js

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,12 +1,17 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { put, takeEvery } from 'redux-saga/effects'
 import {GET_INIT_LIST} from './actionTypes';
 import { initlistAction } from './actionCreators';
 import axios from 'axios';
 
 function* getInitList(){
-    const res= yield axios.get('/todolist.json');
-    const action=initlistAction(res.data);
-    yield put(action);
+    try{
+        const res= yield axios.get('/todolist.json');
+        const action=initlistAction(res.data);
+        yield put(action);
+    }catch(e){
+        console.log('list.json faild')
+    }
+    
 }
 
 //generator函数
